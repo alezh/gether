bc0b8c097629e9e7f9481d4aba34605afdd3d385
alezh <alezh.liu@gmail.com>
2018-05-21 00:55:48 +0800

    Initial commit

---
+++ system/spider/spider.go
@@ -0,0 +1,11 @@
+package spider
+
+//蜘蛛规则
+type Spider struct {
+	Name                   string    //名称
+	Description            string    //详情
+	Limit                  int64     //请求限制
+	Keyin                  string
+
+
+}
